12be6030ee00548b252d21dd6c3c84f53275535e
Barthmozz <barthmozz@gmail.com>
2022-11-27 21:02:00 -0300

    feat: ReturnNegative solution

---
+++ ReturnNegative.cs
@@ -0,0 +1,10 @@
+namespace Codewars
+{
+  public class MakeNegativeClass
+  {
+    public static int MakeNegative(int number)
+    {
+      return number < 0 ? number : number * (-1);
+    }
+  }
+}
